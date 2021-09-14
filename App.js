7d288076aa1f643fa512b33c147c870279b2bbf7
gabriel00011 <gabriel.souza.amaro@hotmail.com>
2021-09-14 19:30:50 -0300

    maps

--- App.js
+++ App.js
@@ -2,12 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Maps from "./Screen/mapsGoogle"
 import Animation from './Screen/LayoutAnimation'
+
 
 export default function App() {
   return (
     <>
-      <Animation />
+      <Maps />
+      {/* <Animation /> */}
     </>
   );
 }
